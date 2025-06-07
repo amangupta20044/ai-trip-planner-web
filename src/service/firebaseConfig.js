7c1af0fd327ba1a1e63e48524987919660e2d7cd
amangupta20044 <a.mann20044@gmail.com>
2025-06-07 14:59:45 +0530

    Initial commit

---
+++ src/service/firebaseConfig.js
@@ -0,0 +1,48 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import {getFirestore} from "firebase/firestore"
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optionals
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBWRsfcIGz2MW7rUaApo6lPDufLUbuP3-g",
+//   authDomain: "aitripplanner-3eea5.firebaseapp.com",
+//   projectId: "aitripplanner-3eea5",
+//   storageBucket: "aitripplanner-3eea5.appspot.com",
+//   messagingSenderId: "715912919515",
+//   appId: "1:715912919515:web:45999bda2793f9806f4ffe",
+//   measurementId: "G-2DCDY4MCNR"
+// };
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+// export const db=getFirestore(app);
+// // const analytics = getAnalytics(app);
+
+//my fb code.................................................
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCqAWPn124Xv2-ZAV6VT-5q73BWz8dqyTo",
+  authDomain: "ai-trip-537ff.firebaseapp.com",
+  projectId: "ai-trip-537ff",
+  storageBucket: "ai-trip-537ff.firebasestorage.app",
+  messagingSenderId: "796724451687",
+  appId: "1:796724451687:web:09a2d2fb63396c70e0f7c2",
+  measurementId: "G-5X7X99L7V3"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db=getFirestore(app);
+//const analytics = getAnalytics(app);
